f9574fdd5c11a5f34c754752b413aa7af3d4c40c
trinadh thatakula <trinadh.thatakula@gmail.com>
2025-06-09 06:09:20 +0530

    Refactor: Update Material 3 to MaterialExpressiveTheme
    This commit updates the project to use `MaterialExpressiveTheme` from Material 3 and upgrades the Material 3 library to version `1.4.0-alpha15`.
    
    Key changes:
    * In `Theme.kt`, `MaterialTheme` has been replaced with `MaterialExpressiveTheme`.
    * The `ExperimentalMaterial3ExpressiveApi` annotation has been added where `MaterialExpressiveTheme` is used.
    * The `libs.versions.toml` file has been updated to reflect the new Material 3 version.

--- app/src/main/java/com/valhalla/bolt/ui/theme/Theme.kt
+++ app/src/main/java/com/valhalla/bolt/ui/theme/Theme.kt
@@ -3,7 +3,11 @@ package com.valhalla.bolt.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
+import androidx.compose.material3.MaterialShapes
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -33,6 +37,7 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun BoltTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -50,7 +55,7 @@ fun BoltTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
+    MaterialExpressiveTheme  (
         colorScheme = colorScheme,
         typography = Typography,
         content = content
