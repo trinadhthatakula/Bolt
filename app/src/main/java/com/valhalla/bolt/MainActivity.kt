2d2e6924dfdcd752f4e0998a7acafd8fe2079248
trinadh thatakula <trinadh.thatakula@gmail.com>
2025-06-09 06:38:11 +0530

    feat: Integrate HomeScreen and FlasherViewModel
    This commit integrates the `HomeScreen` Composable and the `FlasherViewModel` into `MainActivity`.
    
    Key changes:
    * An instance of `FlasherViewModel` is created using the `viewModels()` delegate.
    * The `HomeScreen` Composable is now displayed within the `Scaffold` in `MainActivity`.
    * The `viewModel` is passed as a parameter to the `HomeScreen`.

--- app/src/main/java/com/valhalla/bolt/MainActivity.kt
+++ app/src/main/java/com/valhalla/bolt/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,16 +12,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.valhalla.bolt.ui.screens.HomeScreen
 import com.valhalla.bolt.ui.theme.BoltTheme
+import com.valhalla.bolt.viewModel.FlasherViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: FlasherViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BoltTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    HomeScreen(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        viewModel = viewModel
+                    )
                 }
             }
         }
