aac3052162c1345f497b788410fdf1963696b190
trinadh thatakula <trinadh.thatakula@gmail.com>
2025-07-13 06:28:44 +0530

    feat: Add NavItem model and ViewModel Compose dependency
    This commit introduces a new data class `NavItem` to represent navigation items with a title and icon. It also includes a predefined list of `navItems` for "Flash" and "BackUp".
    
    Additionally, the `androidx.lifecycle.viewmodel.compose` dependency was added to `app/build.gradle.kts` and `gradle/libs.versions.toml`.
    
    Minor formatting changes were made to `AndroidManifest.xml`.

---
+++ app/src/main/java/com/valhalla/bolt/model/NavItem.kt
@@ -0,0 +1,14 @@
+package com.valhalla.bolt.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.valhalla.bolt.R
+
+data class NavItem(
+    val title: String = "Flash",
+    val icon: Int = R.drawable.launcher_foreground,
+)
+
+val navItems = listOf(
+    NavItem("Flash", R.drawable.flash_on),
+    NavItem("BackUp", R.drawable.archive),
+)
